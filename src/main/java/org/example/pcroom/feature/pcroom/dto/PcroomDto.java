c8be9138b561059630266c95471d73ed4cb17a25
suhyeok moon <m51187251@gmail.com>
2025-07-11 13:57:52 +0900

    User DTO 추가

---
+++ src/main/java/org/example/pcroom/feature/pcroom/dto/PcroomDto.java
@@ -0,0 +1,12 @@
+package org.example.pcroom.feature.pcroom.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+public class PcroomDto {
+    private Long id;
+    private String name;
+}
