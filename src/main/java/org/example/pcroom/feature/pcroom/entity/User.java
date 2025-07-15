f3eb4ad8f67a8e65e950dbe0d1a7109a2f227c7d
suhyeok moon <m51187251@gmail.com>
2025-07-15 16:20:58 +0900

    User entity 수정

--- src/main/java/org/example/pcroom/feature/pcroom/entity/User.java
+++ src/main/java/org/example/pcroom/feature/pcroom/entity/User.java
@@ -3,6 +3,7 @@ package org.example.pcroom.feature.pcroom.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
@@ -11,6 +12,7 @@ import java.util.List;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 @Table(name = "users")
 public class User {
     @Id
