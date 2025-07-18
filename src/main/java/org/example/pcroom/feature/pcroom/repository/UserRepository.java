61f1f512a90d4e6f3d93ea0cf1d264379a39b808
suhyeok moon <m51187251@gmail.com>
2025-07-18 16:26:20 +0900

    리포지토리 수정

--- src/main/java/org/example/pcroom/feature/pcroom/repository/UserRepository.java
+++ src/main/java/org/example/pcroom/feature/pcroom/repository/UserRepository.java
@@ -2,8 +2,17 @@ package org.example.pcroom.feature.pcroom.repository;
 
 import org.example.pcroom.feature.pcroom.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUserId(Long userId);
+    List<User> findAll();
+    @Query("SELECT u FROM User u JOIN FETCH u.pcrooms WHERE u.userId = :userId")
+    Optional<User> findByIdWithPcrooms(@Param("userId") Long userId);
 }
