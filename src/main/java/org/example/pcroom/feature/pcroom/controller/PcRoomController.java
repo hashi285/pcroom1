587c0ce551e30f9ba401bc3147dd90e1f28dcb24
suhyeok moon <m51187251@gmail.com>
2025-07-18 16:25:59 +0900

    PcRoomController 메서드 추가 가동률 계산, 다인 이용 추천 알고리즘 구현

--- src/main/java/org/example/pcroom/feature/pcroom/controller/PcRoomController.java
+++ src/main/java/org/example/pcroom/feature/pcroom/controller/PcRoomController.java
@@ -1,18 +1,42 @@
 package org.example.pcroom.feature.pcroom.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.example.pcroom.feature.pcroom.dto.SeatsDto;
 import org.example.pcroom.feature.pcroom.service.PcRoomService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.example.pcroom.feature.pcroom.service.PingService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Map;
 
-
+@RequestMapping("pc")
 @RestController
 @RequiredArgsConstructor
 public class PcRoomController {
     private final PcRoomService pcRoomService;
+    private final PingService pingService;
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "문혁수?!";
+    /**
+     *
+     * @param pcroomid
+     * @return 컴퓨터 생존 여부를 list 형식으로 반환
+     * @throws Exception
+     */
+    @GetMapping("/{pcroomid}")
+    @Operation(summary = "컴퓨터 생존 여부", description = "컴퓨터 생존 여부를 list 형식으로 반환")
+    public List<String> getPcroom(@PathVariable Long pcroomid) throws Exception {
+        return  pingService.update(pcroomid);
+    }
+
+
+    @GetMapping("/pc/{pcroomid}")
+    public String getSeats(@PathVariable Long pcroomid) throws Exception {
+        return pcRoomService.canUseSeat(pcroomid);
+    }
+
+    @GetMapping("/pc/{pcroomid}/{is_available}")
+    public Map<Integer, List<Integer>> getPcRoomSeats(@PathVariable Long pcroomid, Long is_available) throws Exception {
+        return pcRoomService.getIpResult(pcroomid,is_available);
     }
 }
