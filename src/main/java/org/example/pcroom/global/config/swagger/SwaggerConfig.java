7d7c31e1a93a598a90cc60e7dc6acbcbd238cae8
suhyeok moon <m51187251@gmail.com>
2025-07-29 12:30:12 +0900

    feat: SwaggerConfig 추가

---
+++ src/main/java/org/example/pcroom/global/config/swagger/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package org.example.pcroom.global.config.swagger;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("피시방 자리 찾기")
+                        .version("1.0")
+                        .description("피시방 자리를 찾아주는 어플리케이션"));
+    }
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/**")
+                .build();
+    }
+}
