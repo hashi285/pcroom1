e5baddf09736a0e34add33482b850900a1c0a1e6
suhyeok moon <m51187251@gmail.com>
2025-08-27 09:02:18 +0900

    feat: 불필요 클래스 삭제

--- src/main/java/org/example/pcroom/global/config/security/JwtProvider.java
+++
@@ -1,8 +0,0 @@
-package org.example.pcroom.global.config.security;
-
-import org.springframework.stereotype.Component;
-// JWT 생성/검증
-@Component
-public class JwtProvider {
-
-}
--- src/main/java/org/example/pcroom/global/config/security/PasswordEncoder.java
+++
@@ -1,6 +0,0 @@
-package org.example.pcroom.global.config.security;
-// 회원가입 시: 평문 비밀번호 → 암호화 → DB 저장
-
-//로그인 시: 입력 비밀번호 → 암호화 비교 → DB 암호화 비밀번호와 일치 확인
-public class PasswordEncoder {
-}
