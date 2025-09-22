6b22597f41789d6f1dec818467aacad6968e2bf7
suhyeok moon <m51187251@gmail.com>
2025-09-22 11:32:37 +0900

    (admin): 유저 전체 리스트 반환 기능 구현

--- src/main/java/org/example/pcroom/feature/admin/service/AdminService.java
+++ src/main/java/org/example/pcroom/feature/admin/service/AdminService.java
@@ -1,4 +1,32 @@
 package org.example.pcroom.feature.admin.service;
 
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.example.pcroom.feature.admin.dto.UserListDto;
+import org.example.pcroom.feature.user.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class AdminService {
+    private final UserRepository userRepository;
+
+    @Transactional
+    public List<UserListDto> getAllUsers() {
+
+        return userRepository.findAll().stream()
+                .map(user -> new UserListDto(
+                        user.getUserId(),
+                        user.getNickname(),
+                        user.getEmail(),
+                        user.getRole(),
+                        user.getCreateDate()
+
+                ))
+                .toList();
+    }
 }
