a44e7ddb86a75e78746a26caa88f9736eb5926ce
suhyeok moon <m51187251@gmail.com>
2025-09-26 08:43:54 +0900

    모든 Controller swagger tag 추가

--- src/main/java/org/example/pcroom/feature/manager/controller/ManagerController.java
+++ src/main/java/org/example/pcroom/feature/manager/controller/ManagerController.java
@@ -1,4 +1,7 @@
 package org.example.pcroom.feature.manager.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "관리자 + 피시방 매니저 API", description = "앱 관리자 및 피시방 매니저만 사용합니다.")
 public class ManagerController {
 }
