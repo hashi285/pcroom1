8de8c6b3e13333d64adfc3efc9db68d1d316c07f
suhyeok moon <m51187251@gmail.com>
2025-11-16 18:01:20 +0900

    Scheduler 관련 코드 수정

--- src/main/java/org/example/pcroom/feature/pcroom/scheduler/PingScheduler.java
+++ src/main/java/org/example/pcroom/feature/pcroom/scheduler/PingScheduler.java
@@ -3,12 +3,14 @@ package org.example.pcroom.feature.pcroom.scheduler;
 import lombok.RequiredArgsConstructor;
 import org.example.pcroom.feature.pcroom.entity.Pcroom;
 import org.example.pcroom.feature.pcroom.repository.PcroomRepository;
+import org.example.pcroom.feature.pcroom.repository.SeatUsageHourlyRepository;
 import org.example.pcroom.feature.pcroom.service.PingService;
 import org.example.pcroom.feature.pcroom.service.SeatUsageDailyService;
 import org.example.pcroom.feature.pcroom.service.SeatUsageHourlyService;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
 import java.util.List;
 @Component
 @RequiredArgsConstructor
@@ -19,6 +21,7 @@ public class PingScheduler {
     private final SeatUsageHourlyService seatUsageHourlyService;
     private final SeatUsageDailyService seatUsageDailyService;
 
+    private final SeatUsageHourlyRepository seatUsageHourlyRepository;
     /**
      * 30분마다 모든 PC방 좌석별 사용량 기록
      */
@@ -52,8 +55,18 @@ public class PingScheduler {
                 .map(Pcroom::getPcroomId)
                 .toList();
 
+        // 자정 기준으로 '어제' 날짜 계산
+        LocalDate targetDate = LocalDate.now().minusDays(1);
+
         for (Long pcroomId : pcroomIds) {
+            // 1. 하루치 집계 수행
             seatUsageDailyService.aggregateDailyUsage(pcroomId);
+
+            // 2. 집계 완료 후 hourly 데이터 삭제
+            seatUsageHourlyRepository.deleteByPcroomIdAndDate(pcroomId, targetDate);
+
+            System.out.println("Daily 집계 및 hourly 삭제 완료: " + pcroomId + " / " + targetDate);
         }
     }
+
 }
